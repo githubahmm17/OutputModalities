c52cd7270ddb122605e4c15dcaab30e5bfe8b1d9
githubahmm17 <55874247+githubahmm17@users.noreply.github.com>
2019-11-20 19:44:24 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -7,17 +7,15 @@ $(document).on("pagecreate","#pageone",function(){
  	}); 
 });
 
-
-function random() {
-var x = !Math.round(Math.random());
+function random() {	
+var x = !Math.round(Math.random()); //true/false is assigned to x
 $("#myText").text(x);
 
-
 	if(x == true){
-	navigator.notification.beep(1);
+	navigator.notification.beep(1); //one beep
 	}
 	if(x == false){
-	navigator.notification.beep(2);
+	navigator.notification.beep(2); //two beeps
 	}
 	
 	return x;
