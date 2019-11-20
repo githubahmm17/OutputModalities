43dddff0c312fb7da9e6258025091989137320fd
githubahmm17 <55874247+githubahmm17@users.noreply.github.com>
2019-11-20 18:40:19 +0000

    changes to code

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,13 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
-$(document).on("pagecreate","#pageone",function(){
-  
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
+$(document).on("pagecreate","#pageone",function(){  
+                   
 
 	$('#taptext').on("tap",function(){
     	$(this).css('color', 'red');
  	}); 
 
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
+
 
 
 });
