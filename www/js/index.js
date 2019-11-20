3439b4d24b7235caad38ceae5592cd03759ff86e
githubahmm17 <55874247+githubahmm17@users.noreply.github.com>
2019-11-20 19:14:11 +0000

    changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,14 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
-$(document).on("pagecreate","#pageone",function(){  
-                   
-
-	$('#taptext').on("tap",function(){
-    	$(this).css('color', 'red');
+$(document).on("pagecreate","#pageone",function(){                 
+	$('#random').on("tap",function(){
+    random();
  	}); 
-
-
-
-
 });
+
+
+function random() {
+var x = !Math.round(Math.random());
+$("#myText").text(x);
+return x;
+}
